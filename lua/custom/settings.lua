dde152392c58c9c08041bee52d9b3373bc8372e0
Vojtěch Struhár <vojtechstruhar@gmail.com>
2024-03-05 10:17:41 +0100

    Fix tabs

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,3 +1,8 @@
 vim.opt.smartindent = true
 vim.opt.scrolloff = 100 -- typewriter mode :)
 vim.opt.termguicolors = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
